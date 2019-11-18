81c64ecd6d6a0d2ee273b88ac2c0c671f3cd7aeb
Emerson Doyah <emerson.doyah@gmail.com>
2019-11-17 21:00:04 -0600

    refactor

--- src/com/emerson/Survey.java
+++ src/com/emerson/Survey.java
@@ -10,6 +10,34 @@ public class Survey {
     private static final Map<String, int[]> result = new HashMap<>();
     private static final int[] scores = new int[10];
 
+    static Map<String, int[]> takeSurvey() {
+        String name = getUserInfo();
+        takeUserSurveyResponses();
+        result.put(name, scores);
+        return result;
+    }
+
+    private static String getUserInfo() {
+        System.out.println("Take our survey to get matched up with a friend.");
+        System.out.println("Please enter your username.\r");
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name + "!\nPlease enter the number for the best option to each question.\n");
+        return name;
+    }
+
+    private static void takeUserSurveyResponses() {
+        String[] questions = getQuestions();
+        while (questionIndex <= questions.length - 1) {
+            String input = getUserInput(questions);
+            String errorMessage = "\u001B[31m" + input + " is not a valid input. Enter any option from 1 to 5.\n" + "\u001B[0m";
+            try{
+                checkInput(input, errorMessage);
+            }catch (NumberFormatException e){
+                System.out.println(errorMessage);
+            }
+        }
+    }
+
     private static String[] getQuestions() {
         return new String[]{"Your mind is always buzzing with unexplored ideas and plans.",
                 "Generally speaking, you rely more on your experience than your imagination.",
@@ -23,48 +51,6 @@ public class Survey {
                 "You feel more energetic after spending time with a group of people.",};
     }
 
-    static Map<String, int[]> takeSurvey() {
-        String name = getUserInfo();
-        checkResponse();
-        result.put(name, scores);
-        return result;
-    }
-
-    private static String getUserInfo() {
-        System.out.println("Take our survey to get matched up with a friend.");
-        System.out.println("Please enter your username.\r");
-        String name = scanner.nextLine();
-        System.out.println("Welcome " + name + "!\nPlease enter the number for the best option to each question.\n");
-        return name;
-    }
-
-    private static void checkResponse() {
-        String[] questions = getQuestions();
-        while (questionIndex <= questions.length - 1) {
-            String input = getUserInput(questions);
-            String errorMessage = "\u001B[31m" + input + " is not a valid input. Enter any option from 1 to 5.\n" + "\u001B[0m";
-            try{
-                checkInput(input, errorMessage);
-            }catch (NumberFormatException e){
-                System.out.println(errorMessage);
-            }
-        }
-    }
-
-    private static void checkInput(String input, String errorMessage) {
-        int index = Integer.parseInt(input);
-        if(isValidInput(index)) {
-            scores[questionIndex] = index;
-            questionIndex += 1;
-        }else {
-            System.out.println(errorMessage);
-        }
-    }
-
-    private static boolean isValidInput(int index) {
-        return index > 0 && index < 6;
-    }
-
     private static String getUserInput(String[] questions) {
         String question = "#" + (questionIndex + 1) + ". " + questions[questionIndex];
         System.out.println(question);
@@ -74,5 +60,19 @@ public class Survey {
 
     private static String displayOptions() {
         return "1 Strongly Disagreed\n2 Somehow Disagreed\n3 Not too sure\n4 Somehow Agreed\n5 Strongly Agreed";
+    }
+
+    private static void checkInput(String input, String errorMessage) {
+        int optionNumber = Integer.parseInt(input);
+        if(isValidOptionNumber(optionNumber)) {
+            scores[questionIndex] = optionNumber;
+            questionIndex += 1;
+        }else {
+            System.out.println(errorMessage);
+        }
+    }
+
+    private static boolean isValidOptionNumber(int index) {
+        return index > 0 && index < 6;
     }
 }
